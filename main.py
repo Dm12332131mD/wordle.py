a7e7ea440f8ac7c1adbf5ebfa97251f8f55b3aa8
DmmD <dm12332131mdgaming@gmail.com>
2022-02-27 18:55:50 -0500

    Saving

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 # Wordle.py by DmmD
 
-def game():
+# Imports
+import os
+import random
 
-def render():
+# Configurations
+maxTries = 6
+
+# Variables
+with open("texts/answers.txt", "r") as file:
+    answers = file.read().split("\n")
+with open("texts/valid.txt", "r") as file:
+    valid = file.read().split("\n")
+
+# Functions
+def game():
+    guesses, answer = [], random.choice(answers)
+    for i in range(maxTries):
+        while True:
+            render(answer, guesses) 
+            print("Enter below a 5-letter word:")
+            guess = input("> ")
+            if guess in answers or guess in valid:
+                guesses.append(guess)
+                break
+        if guess == answer:
+            render(answer, guesses)
+            print("You won!")
+            input()
+            break
+    if guess != answer:
+        render(answer, guesses)
+        print(f"You lost! The word was: {answer}")
+        input()
+
+def render(answer, guesses):
+    os.system("cls")
+    print("=== [ Wordle.py ] ===\n")
+    for i in range(len(guesses)):
+        characters = []
+        for j in range(5):
+            guess = guesses[i]
+            character = guess[j]
+            if character == answer[j]:
+                characters.append(f"\033[102m\033[30m {character} \033[0m")
+            elif guess.count(character) <= answer.count(character):
+                characters.append(f"\033[103m\033[30m {character} \033[0m")
+            else:
+                characters.append(f"\033[100m\033[37m {character} \033[0m")
+        print(" ".join(characters) + "\n")
+    for i in range(maxTries - len(guesses)):
+        print("\033[100m   \033[0m " * 5 + "\n")
+
+while True:
+    game()
